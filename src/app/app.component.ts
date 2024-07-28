6a6bb873eb7a9b50a139a7b0f3756342f235eca5
ruslan.rudenko <ruslan.rudenko@sigma.software>
2024-07-28 19:40:01 +0200

    add logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Subscription, timer } from 'rxjs';
+
+type Player = 1 | 2;
 
 @Component({
   selector: 'app-root',
@@ -9,14 +12,62 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  currentMove: { Player: 1 | 2 } = { Player: 1 };
+export class AppComponent implements OnInit, OnDestroy {
+  currentPlayer: Player = 1;
+
+  winner: Player | null = null;
+
+  subscription?: Subscription;
+
+  gameover: boolean = true;
+
+  timeToGameOver: number = 0;
+  seconds: number = 0;
+
+  ngOnInit(): void {
+    this.gameover = true;
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  get notCurrentPlayer(): Player {
+    return this.currentPlayer === 1 ? 2 : 1;
+  }
 
   skip(player: 1 | 2) {
-    if (this.currentMove.Player === player) {
-      this.currentMove = {
-        Player: this.currentMove.Player === 1 ? 2 : 1,
-      };
+    if (this.currentPlayer === player) {
+      this.currentPlayer = this.notCurrentPlayer;
     }
+  }
+
+  start(): void {
+    this.timeToGameOver = this.getRandomNumber(3, 5);
+    this.seconds = 0;
+    this.winner = null;
+
+    this.subscription = timer(0, 1000).subscribe(() => {
+      console.warn("running timer", this.timeToGameOver - this.seconds);
+      if (this.seconds > this.timeToGameOver) {
+        this.endGame();
+        this.subscription?.unsubscribe();
+      } else {
+        this.seconds++;
+      }
+    });
+
+    this.gameover = false;
+  }
+
+  private endGame() {
+    this.gameover = true;
+    this.winner = this.notCurrentPlayer;
+  }
+
+  private getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
