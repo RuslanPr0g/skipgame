9beb0f23ae07d8e7c845242d344f3f8440058edc
ruslan.rudenko <ruslan.rudenko@sigma.software>
2024-07-28 19:23:43 +0200

    add base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,16 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'skipgame';
+  currentMove: { Player: 1 | 2 } = { Player: 1 };
+
+  skip(player: 1 | 2) {
+    if (this.currentMove.Player === player) {
+      this.currentMove = {
+        Player: this.currentMove.Player === 1 ? 2 : 1,
+      };
+    }
+  }
 }
